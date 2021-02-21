f84d205eecda52c875bc4649ce152374b642117e
erifranck <erifranjose@hotmail.com>
2021-02-20 19:27:54 -0500

    change app styles and insert new compoentents

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,51 @@
 import React from 'react';
-import {RandomCard} from 'components/RandomCard/RandomCard';
+import { RandomCard } from 'components/RandomCard/RandomCard';
+import { ProfileIcon } from 'components/ProfileIcon/ProfileIcon';
+import { Card } from 'components/Card/Card';
+import characters from 'characters.json';
 import './App.css';
 
-const data: {name: string}[] = [
-  {name: "erifranck"},
-  {name: "kleydith"},
-  {name: "kerlianna"},
-  {name: "anthoner"},
-  {name: "eloy"},
-  {name: "odalis"},
-  {name: "enrique"},
-]
+interface Data {name: string, movie: string, image?: string};
+interface State {
+  selected?: Data
+}
 
-function App() {
-  return (
-    <div className="App">
-      <RandomCard
-        data={data}
-        render={(data: {name?: string}) => (
-          <div>
-            {data.name}
+class App extends React.Component<null, State> {
+  constructor() {
+    super();
+    this.state = {}
+  }
+  render () {
+    const {selected} = this.state;
+    return (
+      <div className="App">
+        <div className="app-content">
+          <ProfileIcon> ? </ProfileIcon>
+          <div class="app-cards">
+            <div className="app-random-card">
+              <RandomCard
+                data={characters}
+                onStop={(value: Data) => {
+                  this.setState({selected: value})
+                }}
+                render={(data: Data) => (
+                  <Card title={data.name} subTitle={data.movie} />
+                )} />
+            </div>
+            <div className="app-random-card">
+              <RandomCard
+                blockStop={!selected}
+                data={characters.filter(character => character.name !== selected?.name)}
+                render={(data: Data) => {
+                  return (
+                    <Card title={data?.name} subTitle={data?.movie} />
+                  )}
+                } />
+            </div>
           </div>
-        )} />
-    </div>
-  );
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
